a694f49849c276df94782f03c73beae33068269f
Ken Brown <kbrown@cornell.edu>
2014-08-19 10:28:07 -0400

    Make set-emacs-default-* change the priorities and stay in auto mode
    Suggested by Achim Gratz (https://cygwin.com/ml/cygwin/2014-08/msg00363.html).

--- set-emacs-default-X11.sh
+++ set-emacs-default-X11.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-
+case $0 in
+    *w32*) prio=25 ;;
+    *X11*) prio=15 ;;
+esac
 for x in emacs emacsclient
 do
-    /usr/sbin/alternatives --set ${x} /usr/bin/${x}-X11.exe
+    /usr/sbin/alternatives --install /usr/bin/${x} ${x} \
+			   /usr/bin/${x}-w32.exe ${prio}
+    /usr/sbin/alternatives --auto ${x}
 done
