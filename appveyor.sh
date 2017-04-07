635931e71900aa85dfa5fce4cac149b13ec4424d
Ken Brown <kbrown@cornell.edu>
2017-04-07 12:05:02 -0400

    Appveyor tweak

--- appveyor.sh
+++ appveyor.sh
@@ -6,8 +6,8 @@ cygport emacs.cygport fetch prep compile || exit_status=1
 
 if [ $exit_status -eq 0 ]
 then
-    cygport emacs.cygport test
     cygport emacs.cygport inst pkg || exit_status=1
+    cygport emacs.cygport test || echo "Test(s) failed."
 fi
 
 tar -cJf artifact.tar.xz emacs-*/dist emacs-*/log
