7a3f263c1001092c79fc6edb2d5ead7e6743c12b
Ken Brown <kbrown@cornell.edu>
2017-04-21 13:21:25 -0400

    Add touch-delete.sh

---
+++ touch-delete.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+
+# Run from dist or release, assuming directory structure is already
+# set up.
+
+touch emacs/-emacs-25.2-0.1.hint
+touch emacs/-emacs-25.2-0.1.tar.xz
+touch emacs/-emacs-25.2-0.1-src.tar.xz
+touch emacs/emacs-debuginfo/-emacs-debuginfo-25.2-0.1.hint
+touch emacs/emacs-debuginfo/-emacs-debuginfo-25.2-0.1.tar.xz
+touch emacs/emacs-el/-emacs-el-25.2-0.1.hint
+touch emacs/emacs-el/-emacs-el-25.2-0.1.tar.xz
+touch emacs/emacs-w32/-emacs-w32-25.2-0.1.hint
+touch emacs/emacs-w32/-emacs-w32-25.2-0.1.tar.xz
+touch emacs/emacs-X11/-emacs-X11-25.2-0.1.hint
+touch emacs/emacs-X11/-emacs-X11-25.2-0.1.tar.xz
