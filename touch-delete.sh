f0c93f70744ee3397e2faf2317244284ab5b1881
Ken Brown <kbrown@cornell.edu>
2019-04-15 15:47:21 -0400

    Update for emacs-26.2-1

--- touch-delete.sh
+++ touch-delete.sh
@@ -3,14 +3,18 @@
 # Run from dist or release, assuming directory structure is already
 # set up.
 
-touch emacs/-emacs-25.2-0.1.hint
-touch emacs/-emacs-25.2-0.1.tar.xz
-touch emacs/-emacs-25.2-0.1-src.tar.xz
-touch emacs/emacs-debuginfo/-emacs-debuginfo-25.2-0.1.hint
-touch emacs/emacs-debuginfo/-emacs-debuginfo-25.2-0.1.tar.xz
-touch emacs/emacs-el/-emacs-el-25.2-0.1.hint
-touch emacs/emacs-el/-emacs-el-25.2-0.1.tar.xz
-touch emacs/emacs-w32/-emacs-w32-25.2-0.1.hint
-touch emacs/emacs-w32/-emacs-w32-25.2-0.1.tar.xz
-touch emacs/emacs-X11/-emacs-X11-25.2-0.1.hint
-touch emacs/emacs-X11/-emacs-X11-25.2-0.1.tar.xz
+touch emacs/-emacs-26.1.92-3.hint
+touch emacs/-emacs-26.1.92-3.tar.xz
+touch emacs/-emacs-26.1.92-3-src.tar.xz
+touch emacs/emacs-common/-emacs-common-26.1.92-3.hint
+touch emacs/emacs-common/-emacs-common-26.1.92-3.tar.xz
+touch emacs/emacs-debuginfo/-emacs-debuginfo-26.1.92-3.hint
+touch emacs/emacs-debuginfo/-emacs-debuginfo-26.1.92-3.tar.xz
+touch emacs/emacs-el/-emacs-el-26.1.92-3.hint
+touch emacs/emacs-el/-emacs-el-26.1.92-3.tar.xz
+touch emacs/emacs-lucid/-emacs-lucid-26.1.92-3.hint
+touch emacs/emacs-lucid/-emacs-lucid-26.1.92-3.tar.xz
+touch emacs/emacs-w32/-emacs-w32-26.1.92-3.hint
+touch emacs/emacs-w32/-emacs-w32-26.1.92-3.tar.xz
+touch emacs/emacs-X11/-emacs-X11-26.1.92-3.hint
+touch emacs/emacs-X11/-emacs-X11-26.1.92-3.tar.xz
